8c5f7a806e46d8d4b3b7f2abef3957bf1edacbb4
Nguyễn Thăng Long <thanglongnguyensp@gmail.com>
2017-03-20 18:19:31 +0700

    updateAndroid_06

---
+++ Android_06_Event_listenner/app/src/main/java/com/example/thanglongsp/android_06_event_listenner/CalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.example.thanglongsp.android_06_event_listenner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
