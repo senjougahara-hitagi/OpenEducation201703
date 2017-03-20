bec5c5ba7667757a5f3f8dcda5fb158ab7a8fdf7
G6Buddy <pbhhedspi@gmail.com>
2017-03-20 18:24:39 +0700

    HomeWork

---
+++ EventAndroid/app/src/main/java/com/example/pbhhe/eventandroid/GoogleActivity.java
@@ -0,0 +1,13 @@
+package com.example.pbhhe.eventandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoogleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google);
+    }
+}
